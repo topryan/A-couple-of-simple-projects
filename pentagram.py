3cadaa5f4574e88177fe0f4ac794b118c02bf27b
topryan <topcurryryan@outlook.com>
2020-02-03 22:25:54 +0800

    the first project to review turtle

---
+++ pentagram.py
@@ -0,0 +1,12 @@
+import turtle
+
+def main():
+    count = 1
+    while count <= 5:
+        count = count + 1
+        turtle.forward(100)
+        turtle.right(144)
+    turtle.exitonclick()
+
+if __name__ == '__main__':
+    main()
